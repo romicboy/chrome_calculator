d0e83363b06f5a6ff71d0b24b97eee8f1fe34e39
romic <romicboy@163.com>
2016-06-12 11:32:54 +0800

    add calculator type

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # chrome_calculator
-这是一个复利计算器chrome插件
+这是一个复利计算器的chrome插件
--- application/options/js/options.js
+++ application/options/js/options.js
@@ -6,14 +6,39 @@
 		
 		this.bindEvent = function() {
 			$("input[type='submit']").click(function(){
-				var principal = $("input[name='principal']").val();
-				var yield = $("input[name='yield']").val();
+				var type = parseInt($("input[name='type']:checked").val());
+				var present = $("input[name='present']").val();
+				var yield = $("input[name='yield']").val()/100;
 				var year = $("input[name='year']").val();
-				var value = principal*Math.pow(yield,year);
-				$("input[name='property']").val(value);
+				var future = $("input[name='future']").val();
+				if(type == 1){
+					$this.setFuture(present*Math.pow(1+yield,year));
+				} else if(type == 2){
+					$this.setPresent(future/Math.pow(1+yield,year));
+				}
 				return false;
 			});
-			
+
+			$("input[name='type']").click(function(){
+				var type = parseInt($("input[name='type']:checked").val());
+				$("input[name='future']").removeAttr("disabled");
+				$("input[name='present']").removeAttr("disabled");
+				$("input[name='present']").val('');
+				$("input[name='future']").val('');
+				if(type == 1){
+					$("input[name='future']").attr('disabled', 'disabled');
+				} else if(type == 2){
+					$("input[name='present']").attr('disabled', 'disabled');
+				}
+			});
+		};
+
+		this.setPresent = function(present) {
+			$("input[name='present']").val(present);
+		};
+
+		this.setFuture = function(future) {
+			$("input[name='future']").val(future);
 		};
 		
 		this.init = function() {
